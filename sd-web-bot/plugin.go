fbee00f5237761e3311f3fea6be73cc1261de43a
www.cnm.sb <70825251+cnm-sb@users.noreply.github.com>
2024-09-24 22:03:38 +0800

    Update plugin.go

--- sd-web-bot/plugin.go
+++ sd-web-bot/plugin.go
@@ -1,1 +1,9 @@
-a
+package plugininterface
+
+import "github.com/gin-gonic/gin"
+
+// PluginInterface 定义插件接口
+type PluginInterface interface {
+	RegisterRoutes() []gin.RouteInfo
+	GetName() string
+}
